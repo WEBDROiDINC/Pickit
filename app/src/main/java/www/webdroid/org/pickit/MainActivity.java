098c34ceaad2124c5c6ab741473ab15c4484029d
WEBDROiDINK <35076248+WEBDROiDINK@users.noreply.github.com>
2018-01-27 21:28:38 +0200

    remove fab button

--- app/src/main/java/www/webdroid/org/pickit/MainActivity.java
+++ app/src/main/java/www/webdroid/org/pickit/MainActivity.java
@@ -1,5 +1,6 @@
 package www.webdroid.org.pickit;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -12,16 +13,28 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
+    Button findPlaceButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        findPlaceButton = (Button) findViewById(R.id.buttonFindaplaceMain);
+        findPlaceButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent i = new Intent( MainActivity.this, Main2Activity.class);
+                startActivity(i);
+            }
+        });
 /*
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
---
+++ app/src/main/java/www/webdroid/org/pickit/SplashScreen.java
@@ -0,0 +1,13 @@
+package www.webdroid.org.pickit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+    }
+}
