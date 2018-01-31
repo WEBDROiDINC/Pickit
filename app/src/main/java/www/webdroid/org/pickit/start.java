9c6e2bb0e9d398d220aaace8a330bae6f29ac908
WEBDROiDINK <35076248+WEBDROiDINK@users.noreply.github.com>
2018-02-01 01:57:52 +0200

    add start page --- add 2 custom page --- button desgin -- new logo --- edit splashScreen

--- app/src/main/java/www/webdroid/org/pickit/start.java
+++ app/src/main/java/www/webdroid/org/pickit/start.java
@@ -3,11 +3,11 @@ package www.webdroid.org.pickit;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class Main2Activity extends AppCompatActivity {
+public class start extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
+        setContentView(R.layout.activity_start);
     }
 }
