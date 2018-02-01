9213eb88723d096838a8cd768cb6d90457ac78b3
WEBDROiDINK <35076248+WEBDROiDINK@users.noreply.github.com>
2018-02-01 03:51:11 +0200

    edit start page // add buttons add new drawable

--- app/src/main/java/www/webdroid/org/pickit/SplashScreen.java
+++ app/src/main/java/www/webdroid/org/pickit/SplashScreen.java
@@ -1,7 +1,9 @@
 package www.webdroid.org.pickit;
 
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class SplashScreen extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        TextView myTextView = (TextView) findViewById(R.id.textView);
+        Typeface typeface = Typeface.createFromAsset(getAssets(), "fonts/Shrikhand-Regular.ttf");
+        myTextView.setTypeface(typeface);
+
     }
 }
--- app/src/main/java/www/webdroid/org/pickit/start.java
+++ app/src/main/java/www/webdroid/org/pickit/start.java
@@ -1,13 +1,37 @@
 package www.webdroid.org.pickit;
 
+import android.graphics.Typeface;
+import android.os.Build;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.TextView;
 
 public class start extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_start);
+
+        setContentView(R.layout.activity_start); TextView myTextView = (TextView) findViewById(R.id.textView);
+        Typeface typeface = Typeface.createFromAsset(getAssets(), "fonts/Shrikhand-Regular.ttf");
+        myTextView.setTypeface(typeface);
+
+
+
+        Window window = this.getWindow();
+
+// clear FLAG_TRANSLUCENT_STATUS flag:
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+
+// add FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS flag to the window
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+
+// finally change the color
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            window.setStatusBarColor(ContextCompat.getColor(this,R.color.colorStatusBarStart));
+        }
     }
 }
