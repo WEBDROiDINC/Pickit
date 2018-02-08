56502b122fd1dba743b45f98f9e92bda2bcfe8cd
WEBDROiDINK <35076248+WEBDROiDINK@users.noreply.github.com>
2018-02-08 18:17:23 +0200

    font assets new splash desgin and animation new start desgin rename activity add anim 3 files

--- app/src/main/java/www/webdroid/org/pickit/SplashScreen.java
+++ app/src/main/java/www/webdroid/org/pickit/SplashScreen.java
@@ -1,20 +1,45 @@
 package www.webdroid.org.pickit;
 
+import android.content.Intent;
 import android.graphics.Typeface;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.TextView;
 
 public class SplashScreen extends AppCompatActivity {
+
+    private static int SPLASH_TIME_OUT = 2000;
+    Animation fade;
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
 
-        TextView myTextView = (TextView) findViewById(R.id.textView);
+        textView = (TextView)findViewById(R.id.textViewSplash);
+
+        fade = AnimationUtils.loadAnimation(this,R.anim.fade);
+
+        textView.setAnimation(fade);
+
+        TextView myTextView = (TextView) findViewById(R.id.textViewSplash);
         Typeface typeface = Typeface.createFromAsset(getAssets(), "fonts/Shrikhand-Regular.ttf");
         myTextView.setTypeface(typeface);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                Intent gto = new Intent(SplashScreen.this,StartActivity.class);
+                startActivity(gto);
+
+                finish();
+            }
+        },SPLASH_TIME_OUT);
 
     }
 }
--- app/src/main/java/www/webdroid/org/pickit/StartActivity.java
+++ app/src/main/java/www/webdroid/org/pickit/StartActivity.java
@@ -1,24 +1,45 @@
 package www.webdroid.org.pickit;
 
+import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Build;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.TextView;
 
-public class start extends AppCompatActivity {
+public class StartActivity extends AppCompatActivity {
+
+    Button signbtn,startbtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.activity_start); TextView myTextView = (TextView) findViewById(R.id.textView);
+      setContentView(R.layout.activity_start);
+
+      signbtn= (Button)findViewById(R.id.buttonSignUpStart);
+      startbtn = (Button)findViewById(R.id.buttonGetstartedStart);
+
+      startbtn.setOnClickListener(new View.OnClickListener() {
+          @Override
+          public void onClick(View v) {
+              Intent gto = new Intent(StartActivity.this,MainActivity.class);
+              startActivity(gto);
+
+              finish();
+          }
+      });
+
+      ///////////////////////////// FONT ///////////////////////////////////////////////////////////
+      TextView myTextView = (TextView) findViewById(R.id.textView);
         Typeface typeface = Typeface.createFromAsset(getAssets(), "fonts/Shrikhand-Regular.ttf");
         myTextView.setTypeface(typeface);
-
+      //////////////////////////////////////////////////////////////////////////////////////////////
 
 
         Window window = this.getWindow();
@@ -31,7 +52,7 @@ public class start extends AppCompatActivity {
 
 // finally change the color
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            window.setStatusBarColor(ContextCompat.getColor(this,R.color.colorStatusBarStart));
+            window.setStatusBarColor(ContextCompat.getColor(this,R.color.colorPrimaryDark));
         }
     }
 }
